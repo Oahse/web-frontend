3ddf1fed39fc51de41b4fb9c7ebc2de603be74e3
oscaroguledo <oscarchiagoziem@gmail.com>
2024-09-23 00:50:19 +0100

    fixed header bottom navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'; // useLocation to detect URL changes
 import Logo from '../assets/oahse-logo.png';
-import BottomNavbar from './BottomNavBar'; // Import your BottomNavbar component
+import BottomNavbar from './BottomNavBar';
 import { isDesktop, isMobile, isTablet } from 'react-device-detect';
 
 const Header = ({ Companyname, isloggedIn, userDetails }) => {
+    const location = useLocation(); // Get the current location (URL)
+    const [activeLink, setActiveLink] = useState(location.pathname); // Track the active link based on the current path
+
+    // Update active link based on the URL change
+    useEffect(() => {
+        setActiveLink(location.pathname); // Set the active link to the current URL path
+    }, [location]); // Only run when the URL changes
+
     const renderLinks = () => {
         const links = [
             { name: 'Explore', url: '/', icon: <i className="fa-light fa-compass nav-icons"></i> },
             { name: 'MarketPlace', url: '/marketplace', icon: <i className="fa-light fa-grid-2 nav-icons"></i> },
-            { name: 'Map', url: '/map', icon: <i className="fa-light fa-location-crosshairs nav-icons" ></i> },
+            { name: 'Map', url: '/map', icon: <i className="fa-light fa-location-crosshairs nav-icons"></i> },
             { name: 'Cart', url: '/cart', icon: <i className="fa-light fa-cart-shopping nav-icons"></i> },
-            { name: 'User', url: '/user', icon: <i className="fa-light fa-user nav-icons" ></i> },
+            { name: 'User', url: '/user', icon: <i className="fa-light fa-user nav-icons"></i> },
         ];
 
-        return (
-            links.map((link, index) => (
-                link !== 'User' ?<Link key={index} to={link.url} className="m-2 icon-container">
+        return links.map((link, index) => (
+            <Link 
+                key={index} 
+                to={link.url} 
+                className="m-2 icon-container"
+            >
                 <div className="d-flex align-items-center">
-                    {link.icon}
+                    {/* Conditionally apply 'text-white fw-bold' to the active link */}
+                    
+                    {isDesktop ?
+                        <i className={`${link.icon.props.className} ${activeLink === link.url ? 'text-dark fw-bold' : ''}`}></i>: 
+                        <i className={`${link.icon.props.className} ${activeLink === link.url ? 'text-white fw-bold' : ''}`}></i>}
                     {isDesktop && <span className='text-dark ms-1'>{link.name}</span>}
                 </div>
-            </Link>:<Link key={index} to={link.url} className="m-2 icon-container">
-                    <div className="d-flex align-items-center">
-                        {userDetails?userDetails?.profileImage:link.icon}
-                        {isDesktop && <span className='text-dark ms-1'>{link.name}</span>}
-                    </div>
-                </Link>
-            ))
-        );
+            </Link>
+        ));
     };
-
 
     return (
         <>
@@ -42,23 +50,24 @@ const Header = ({ Companyname, isloggedIn, userDetails }) => {
                         <span className="navbar-title"> {Companyname}</span>
                     </Navbar.Brand>
                     <div className="d-flex justify-content-between align-items-center" style={{ marginLeft: 'auto' }}>
-                        {isloggedIn ? 
-                        <span className="d-flex flex-row align-items-center">
-                            <i class="fa-light fa-message-lines nav-icons"></i>
-                            {isDesktop && <span className='text-dark ms-1'>Messages</span>}
-                        </span>
-                        :
-                        (isDesktop && <span className="d-flex flex-row align-items-center">
-                            {renderLinks()}
-                            <Link key={4} to={'/messages'} className="m-2 icon-container">
-                                <div className="d-flex align-items-center">
-                                    <i class="fa-light fa-message-lines nav-icons"></i>
-                                    <span className='text-dark ms-1'>Messages</span>
-                                </div>
-                            </Link></span>)
-                        }
-                        
-                        
+                        {!isloggedIn ? (
+                            <span className="d-flex flex-row align-items-center">
+                                <i className="fa-light fa-message-lines nav-icons"></i>
+                                {isDesktop && <span className='text-dark ms-1'>Messages</span>}
+                            </span>
+                        ) : (
+                            isDesktop && (
+                                <span className="d-flex flex-row align-items-center">
+                                    {renderLinks()}
+                                    <Link to="/messages" className="m-2 icon-container">
+                                        <div className="d-flex align-items-center">
+                                            <i className="fa-light fa-message-lines nav-icons"></i>
+                                            <span className='text-dark ms-1'>Messages</span>
+                                        </div>
+                                    </Link>
+                                </span>
+                            )
+                        )}
                     </div>
                 </Container>
             </Navbar>
