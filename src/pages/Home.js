27d4160506c01ee44b1feb7970441ffdea1ea9c1
oscaroguledo <oscarchiagoziem@gmail.com>
2024-12-11 20:22:01 +0000

    started another box ontop footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -71,9 +71,21 @@ function Homepage({ Companyname }) {
             </span>
             
         </div>
-      
+          
         {/* Other content */}
         <div className='homepage-bottom'>
+          <div className='homepage-aiestimator row m-0'>
+                <div className='col-4 d-flex align-items-center justify-content-center bg-none'>
+                  <svg className='align-self-center' width="251" height="150" viewBox="0 0 251 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M112.5 54.725V42.5C112.5 28.3563 112.5 21.2875 107.656 16.8938C102.806 12.5 95.0125 12.5 79.4062 12.5H51.8438C36.2375 12.5 28.4438 12.5 23.5938 16.8938C18.7438 21.2875 18.75 28.3563 18.75 42.5V82.5C18.75 96.6437 18.75 103.713 23.5938 108.106C28.4438 112.5 36.2375 112.5 51.8438 112.5H79.4062M37.5 37.5H93.75M37.5 62.5H43.75M62.5 62.5H68.75M87.5 62.5H93.75M37.5 87.5H43.75M62.5 87.5H68.75M129.412 93.775C127.7 89.4312 123.288 84.3375 113.25 84.3375C101.588 84.3375 96.675 89.6812 95.6813 92.5375C94.125 96.3562 93.9813 104.6 108.113 105.069C124.988 105.631 132.05 107.925 131.175 117.175C130.306 126.425 120.581 127.712 113.25 128.212C105.719 127.994 96.4063 126.419 93.75 118.431M112.462 75V83.975M112.519 128.181V137.5" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M151 109.666V79.4163C151 76.7421 152.317 74.1773 154.661 72.2863C157.005 70.3954 160.185 69.333 163.5 69.333C166.815 69.333 169.995 70.3954 172.339 72.2863C174.683 74.1773 176 76.7421 176 79.4163V109.666M151 94.5413H176M201 69.333V109.666" stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+
+                </div>
+                <div className='col-8 p-4 text-center green'>
+                      dsd
+                </div>
+          </div>
           <Footer />
         </div>
     </div>
