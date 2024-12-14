47ecc6d000728c63c96910428e507cbd1494c180
oscaroguledo <oscarchiagoziem@gmail.com>
2024-12-14 06:40:44 +0000

    updated desktop version of  testmonies

--- src/components/ui/HomePage/Testimonies/Testimonies.jsx
+++ src/components/ui/HomePage/Testimonies/Testimonies.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Avatar, Button } from 'antd';
+import { Row, Col, Avatar } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import './Testimonies.css';
 import Text from '../../Typography/Text';
 import Card from '../../Card/Card';
+import Button from '../../Button/Button'
 
 const HomepageTestimonies = ({ isMobile }) => {
   // Array of testimonies (just as an example)
   const testimonies = [
+    {
+      name: "John Doe",
+      title: "Founder of ABC Corp.",
+      text: "OAHSE has been a game changer in how I source and manage resources. Highly recommend!"
+    },
     {
       name: "Mayank Balakrishnan",
       title: "Founder of Fetherstill Inc.",
@@ -40,57 +46,118 @@ const HomepageTestimonies = ({ isMobile }) => {
   };
 
   const currentTestimony = testimonies[currentTestimonyIndex];
-
+  document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.scrollable-homepage-testimonies-container');
+  
+    let isDragging = false;
+    let startX, scrollLeft;
+  
+    container.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      container.classList.add('active'); // Optional: Add a class for active drag styling
+      startX = e.pageX - container.offsetLeft;
+      scrollLeft = container.scrollLeft;
+    });
+  
+    container.addEventListener('mouseleave', () => {
+      isDragging = false;
+      container.classList.remove('active');
+    });
+  
+    container.addEventListener('mouseup', () => {
+      isDragging = false;
+      container.classList.remove('active');
+    });
+  
+    container.addEventListener('mousemove', (e) => {
+      if (!isDragging) return; // Exit if not dragging
+      e.preventDefault();
+      const x = e.pageX - container.offsetLeft;
+      const walk = (x - startX) * 2; // Adjust scrolling speed by multiplying
+      container.scrollLeft = scrollLeft - walk;
+    });
+  });
+  
   return (
-    <div className="p-2">
-      <Card className="homepage-testimonies row m-1 mb-3" cardBodyClass="row">
-        <div className="col-12 col-md-6">
-          <div className="homepage-testimonies-content d-flex flex-row align-items-center">
-            <Avatar
-              shape="circle"
-              size={isMobile ? 70 : 130}
-              alt="Testimony Image"
-              className="m-1 homepage-testimonies-image"
-            />
-            <span className="homepage-testimonies-name">
-              <Text fontColor="text-white" fontSize="fs-md" fontWeight="fw-500">
-                {currentTestimony.name}
-              </Text>
-              <Text tag="small" fontSize="fs-sm" fontColor="text-link" fontWeight="fw-200">
-                {currentTestimony.title}
-              </Text>
-            </span>
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="homepage-testimonies-writeup">
-            <Text fontWeight="fw-300" fontSize="fs-md" fontColor="text-white">
-              “{currentTestimony.text}”
-            </Text>
-          </div>
-        </div>
-      </Card>
-
-      {/* Navigation Buttons */}
-      <div className="navigation-buttons d-flex justify-content-between">
-        <Button
-          icon={<LeftOutlined />}
-          onClick={prevTestimony}
-          disabled={currentTestimonyIndex === 0}
-          className="prev-button"
-        >
-          Prev
-        </Button>
-        <Button
-          icon={<RightOutlined />}
-          onClick={nextTestimony}
-          disabled={currentTestimonyIndex === testimonies.length - 1}
-          className="next-button"
-        >
-          Next
-        </Button>
+    
+    <>
+      {!isMobile ? 
+      <div className={`p-2 mb-4 scrollable-homepage-testimonies-container`}>
+        {testimonies.map((testimony, index) => (
+          <Card
+            key={index}
+            className="homepage-testimonies row m-auto"
+            cardBodyClass="row"
+          >
+            <div className="col-12 col-md-6">
+              <div className="homepage-testimonies-content d-flex flex-row align-items-center">
+                <Avatar
+                  shape="circle"
+                  size={isMobile ? 70 : 130}
+                  alt="Testimony Image"
+                  className="m-1 homepage-testimonies-image"
+                />
+                <span className="homepage-testimonies-name">
+                  <Text fontColor="text-white" fontSize="fs-md" fontWeight="fw-500">
+                    {testimony.name}
+                  </Text>
+                  <Text tag="small" fontSize="fs-sm" fontColor="text-link" fontWeight="fw-200">
+                    {testimony.title}
+                  </Text>
+                </span>
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className="homepage-testimonies-writeup">
+                <Text fontWeight="fw-300" fontSize="fs-md" fontColor="text-white">
+                  “{testimony.text}”
+                </Text>
+              </div>
+            </div>
+          </Card>
+        ))}
       </div>
-    </div>
+ : 
+      <div className="p-2 mb-4">
+        <Card className="homepage-testimonies row m-auto" cardBodyClass="row">
+          <div className="col-12 col-md-6">
+            <div className="homepage-testimonies-content d-flex flex-row align-items-center">
+              <Avatar
+                shape="circle"
+                size={isMobile ? 70 : 130}
+                alt="Testimony Image"
+                className="m-1 homepage-testimonies-image"
+              />
+              <span className="homepage-testimonies-name">
+                <Text fontColor="text-white" fontSize="fs-md" fontWeight="fw-500">
+                  {currentTestimony.name}
+                </Text>
+                <Text tag="small" fontSize="fs-sm" fontColor="text-link" fontWeight="fw-200">
+                  {currentTestimony.title}
+                </Text>
+              </span>
+            </div>
+          </div>
+          <div className="col-12 col-md-6">
+            <div className="homepage-testimonies-writeup">
+              <Text fontWeight="fw-300" fontSize="fs-md" fontColor="text-white">
+                “{currentTestimony.text}”
+              </Text>
+            </div>
+          </div>
+        </Card>
+
+        {/* Navigation Buttons */}
+        <div className="navigation-buttons d-flex justify-content-between">
+          
+          <i className="fa-light fa-chevron-left prev-button" onClick={prevTestimony}
+            disabled={currentTestimonyIndex === 0}></i>
+          <i className="fa-light fa-chevron-right next-button" onClick={nextTestimony}
+            disabled={currentTestimonyIndex === testimonies.length - 1}></i>
+          
+        </div>
+      </div>}
+    </>
   );
 };
 
