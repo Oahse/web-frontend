113c737778c14a7db2688ef9ecee48d8a675dd39
oscaroguledo <oscarchiagoziem@gmail.com>
2025-08-19 18:54:36 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Inspect from 'vite-plugin-inspect';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,7 +22,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Example: '@' to point to src
-      '@': path.resolve(__dirname, 'src'),
+      '@': '/src',
     },
   },
 })
